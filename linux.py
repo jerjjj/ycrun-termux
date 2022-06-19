80beeab29cb16313cc9a41aee6ea610bb44aba27
jerjjj <wuzhijun@jerjjj.cn>
2022-06-19 12:45:31 +0800

    Add files via upload

--- linux.py
+++ linux.py
@@ -13,15 +13,27 @@ if a<1:#判断用户输入是否超出范围
 elif a>6:
     exit("请输入正确的序号")
 elif a==1:
-    os.system('sudo sh /data/adb/modules/uperf/script/powercfg_main.sh auto')#切换到自动模式。
+    e = os.system('sudo sh /data/adb/modules/uperf/script/powercfg_main.sh auto')#切换到自动模式。
 elif a==2:
-    os.system('sudo sh /data/adb/modules/uperf/script/powercfg_main.sh balance')#切换到均衡模式
+    e = os.system('sudo sh /data/adb/modules/uperf/script/powercfg_main.sh balance')#切换到均衡模式
 elif a==3:
-    os.system('sudo sh /data/adb/modules/uperf/script/powercfg_main.sh powersave')#切换到卡顿模式
+    e = os.system('sudo sh /data/adb/modules/uperf/script/powercfg_main.sh powersave')#切换到卡顿模式
 elif a==4:
-    os.system('sudo sh /data/adb/modules/uperf/script/powercfg_main.sh performance')#切换到费电模式
+    e = os.system('sudo sh /data/adb/modules/uperf/script/powercfg_main.sh performance')#切换到费电模式
 elif a==5:
-    os.system('sudo sh /data/adb/modules/uperf/script/powercfg_main.sh fast')#切换到极速模式
+    e = os.system('sudo sh /data/adb/modules/uperf/script/powercfg_main.sh fast')#切换到极速模式
+if a != 6:
+    if e == 32512:#如果报错
+        g = int(input("检测到你未安装tsu,是否需要安装(1/0)"))#尝试安装tsu
+        if g == 1:
+            os.system('pkg install tsu')#安装tsu
+            exit()
+        elif g == 0:
+            exit()
+        elif g != 1:#序号错误
+            exit("请输入正确的序号")
+        elif g != 0:
+            exit("请输入正确的序号")
 if a!=6:
     b=input("已经成功切换了,按任意键退出")
 if a==6:
